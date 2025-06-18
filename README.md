8a4b01ffed955ac0a18f66ece8ff619bd0a4ddba
snizar007 <snizar@microsoft.com>
2025-06-18 12:34:17 -0700

    additional update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Python_Begin
 Python Beginner Project
+
+Python Resources:
+
+https://www.python.org/shell/
+
+https://www.onlinegdb.com/online_python_interpreter
+
+https://repl.it/languages/python3
+
+https://www.tutorialspoint.com/execute_python3_online.php
+
+https://rextester.com/l/python3_online_compiler
+
+https://trinket.io/python3
+
--- sample.py
+++ sample.py
@@ -1,1 +1,16 @@
 print("Hello, World!")
+#
+# Define a list of friends and create a greeting
+friends = ("Sam", "Gee", "Aadi")
+for friend in friends:
+    print("Hey " + friend)
+    
+# different way to specify
+friends2 = ["Sam", "Gee", "Aadi"]
+for friend2 in friends2:
+    print("Hey " + friend2)
+
+#
+# Print Hello World 10-times
+for i in range(10):
+  print("Hello, World!")
