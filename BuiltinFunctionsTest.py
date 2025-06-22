23290be3ba086463c5d418497aeba22b9eed71c1
snizar007 <snizar@microsoft.com>
2025-06-22 12:26:24 -0700

    new files

---
+++ BuiltinFunctionsTest.py
@@ -0,0 +1,9 @@
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print("Unsorted list: ", time_list)
+print("Sorted list: ", sorted(time_list))
+
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print("Min list: ", min(time_list))
+print("Max list: ", max(time_list))
--- FunctionsGroup.py
+++ FunctionsGroup.py
@@ -5,3 +5,69 @@ def to_celsius(x):
 y = to_celsius(75)
 
 print (round(y))
+
+def greeting(name):
+    print("Welcome, " + name)
+    
+greeting("Sam")
+
+#
+# Function area_triangle
+
+def area_triangle(base, height):
+    return base*height/2
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+sum = area_a + area_b
+print("The sum of both areas is: " + str(sum))
+
+#
+# Non-reusable example
+name = "Kay"
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+name = "Cameron"
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+#
+# Code Reuse Examples
+def lucky_number(name):
+    number = len(name) * 9
+    print("Hello " + name + ". Your lucky number is " + str(number))
+
+lucky_number("Kay")
+lucky_number("Cameron")
+
+#
+# Coding Style
+def circle_area(radius):
+    pi = 3.14
+    area = pi * (radius ** 2)
+    print(area)
+
+circle_area(5)
+#Output is 78.5
+
+# This function converts fluid ounces to milliliters and returns the 
+# result of the conversion.
+def convert_volume(fluid_ounce):
+# Calculate value of the "ml" variable using the parameter variable 
+# "fluid_ounce". There are approximately 29.5 milliliters in 1 fluid
+# ounce.
+    ml = fluid_ounce * 29.5  
+# Return the result of the calculation.  
+    return ml
+ 
+# Call the conversion from within the print() function using 2 fluid 
+# ounces. Convert the return value from a float to a string.  
+print("The volume of 2.oz in milliliters is " + str(convert_volume(2)))
+ 
+# Call the function again and double the 2 fluid ounces from within
+# the print function.
+print("The volume of 2.oz in milliliters is " + str(convert_volume(2)*2))
+# Alternative calculation:
+# print("The volume in milliliters is " + str(convert_volume(4))
